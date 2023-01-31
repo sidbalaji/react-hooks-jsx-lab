7034b44180bec437d71c9ae8786f150837bfffcf
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-01-30 23:19:35 -0500

    tests should be passing now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const h1Color = {
+  color:'firebrick'
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id = 'home'>Home
+      <h1 style={h1Color}>{name} is a Web Developer from Your {city}</h1>
+    </div>);
 }
 
 export default Home;
